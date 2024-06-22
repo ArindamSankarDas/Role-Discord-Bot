f2497ccaa9a5a5887159b40c7f0004f2e8b28b85
Arindam Sankar Das <arindamsankar@outlook.com>
2024-06-22 21:21:25 +0530

    1. Added a new GatewayIntent to handle guildMembers 2. Made sure to allocate a User role to any new member who joins. 3. Making sure the bot reacts to hello messages to only non-moderator users. 4. Created a command to to delete all messages, which is only accessible to a moderator. 5. Removed Http create server as it won't be hosted for some time.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,102 @@
-const http = require("node:http");
-
 const { Events, Client, GatewayIntentBits } = require("discord.js");
+
+const {
+  channelAsyncErrorHandler,
+  generalAsyncErrorHandler,
+} = require("./utility/asyncErrorHandlers");
+const { BaseAsyncError } = require("./utility/errorHandler");
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
   ],
 });
 
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+/* message creations */
+
+// Welcome the user to the server
 client.on(Events.MessageCreate, (message) => {
   if (message.author.bot) return;
 
-  if (message.content === "hello" || "hi") {
+  const roles = message.member.roles.cache.map((role) =>
+    role.name.toLowerCase()
+  );
+
+  const regex = /\b(hi|hello)\b/i;
+
+  if (regex.test(message.content)) {
+    if (roles.includes("moderator")) return;
+
     message.channel.send("Welcome to the server " + message.author.username);
   }
 });
 
-client
-  .login(process.env.APP_TOKEN)
-  .then(() => {
-    client.once(Events.ClientReady, (readyClient) => {
-      console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+// Delete all messages from the server
+client.on(Events.MessageCreate, async (message) => {
+  if (message.author.bot) return;
 
-      http.createServer().listen(3500, () => {
-        console.log("Server has started running on port 3500");
-      });
-    });
+  const roles = message.member.roles.cache.map((role) =>
+    role.name.toLowerCase()
+  );
+
+  if (message.content === "!delete") {
+    if (!roles.includes("moderator")) {
+      return message.reply(`You are not authorised to execute this command`);
+    }
+
+    message.guild.channels.cache.forEach(
+      channelAsyncErrorHandler(async (channel) => {
+        if (channel.isTextBased()) {
+          let fetched;
+
+          do {
+            fetched = await channel.messages.fetch({ limit: 100 });
+
+            for (const msg of fetched.values()) {
+              await msg.delete().catch((error) => {
+                if (error.code !== 10008) {
+                  console.log(
+                    `Could not delete message ${msg.id} in ${channel.name}: `
+                  );
+
+                  throw new BaseAsyncError(error.name, error.message);
+                }
+              });
+            }
+          } while (fetched.size >= 2);
+
+          console.log("All messages deleted.");
+        }
+      })
+    );
+  }
+});
+
+// Add User role to the new member
+client.on(
+  Events.GuildMemberAdd,
+  generalAsyncErrorHandler(async (member) => {
+    const guild = member.guild;
+
+    const role = guild.roles.cache.find(
+      (role) => role.name.toLowerCase() === "user"
+    );
+
+    if (role) {
+      await member.roles.add(role);
+      console.log(`Added role ${role.name} to ${member.user.tag}`);
+    } else {
+      console.log(`Role ${ROLE_NAME} not found.`);
+    }
   })
-  .catch((err) => {
-    console.error(err);
-  });
+);
+
+// login
+client.login(process.env.APP_TOKEN);
